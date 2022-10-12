2c78363e2a3e2e9c3375cc8b29fe91314685dfc8
Laurids Radtke <laurids.radtke1@gmail.com>
2022-10-12 20:17:13 +0200

    Added button

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 class Ui:
-    def __init__(self,root) -> None:
+    def __init__(self, root) -> None:
         root.title("Scrabble helper")
 
         mainframe = ttk.Frame(root, padding="5 5 5 5")
@@ -9,5 +9,8 @@ class Ui:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        search_button = ttk.Button(mainframe)
-        search_button.grid(column=3, row=2)
+        search_button = ttk.Button(mainframe, text='Search')
+        search_button.grid(column=2, row=1)
+
+        entry_field = ttk.Entry(mainframe)
+        entry_field.grid(column=1, row=1)
